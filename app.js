aedeb9932bbd701b3cde722696cfea7dc0d39cb4
Daniel Gould <dan@dannygould.com>
2016-07-25 15:25:58 -0400

    initialized server with static index

--- app.js
+++ app.js
@@ -4,7 +4,20 @@
 
 var express		= require('express');
 var app			= express();	
+var bodyParser	= require('body-parser');
 
-var port	
+var morgan		= require('morgan');
+
+var server		= require('http').createServer(app);
+var port		= process.env.port || 8080
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+app.use(express.static(__dirname + '/fe'));
+
+server.listen(8080);
+console.log('Magic happens on port %s', port);
+
