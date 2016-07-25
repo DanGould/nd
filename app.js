5bd759507f58d4d501763bc9301063b726cbea3b
Daniel Gould <dan@dannygould.com>
2016-07-25 15:07:41 -0400

    npm init

---
+++ app.js
@@ -0,0 +1,10 @@
+// NS
+// A collaborative project between DG + NS
+// Who knows what this will turn into.
+
+var express		= require('express');
+var app			= express();	
+
+var port	
+
+app.use(morgan('dev'));
